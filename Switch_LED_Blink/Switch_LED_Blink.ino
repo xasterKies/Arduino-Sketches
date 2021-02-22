afb61e3c222c07d06c379a713a86b5bb714e6560
TiokengSamuel <samueledward40@gmail.com>
2021-02-22 16:15:59 +0100

    only when pressed

--- Switch_LED_Blink/Switch_LED_Blink.ino
+++ Switch_LED_Blink/Switch_LED_Blink.ino
@@ -2,7 +2,9 @@
 
 int switchPin = 8; //define switch pin
 int LEDpin = 4;    //define LED pin
-int reading;      //define reading as an integer
+int reading;   //define reading as an integer
+int switchState = LOW;
+int LEDState = LOW;
 
 void setup() {
   pinMode(LEDpin, OUTPUT);
@@ -10,5 +12,8 @@ void setup() {
 
 void loop() {
   reading = digitalRead(switchPin);  //read switch pin
+  if(reading == HIGH && switchState == LOW) LEDState = !LEDState;
   digitalWrite(LEDpin, reading);     //turn LED on if switch is HIGH
+  switchState = reading;
+  
 }                                    //turn LED off if switch is LOW
