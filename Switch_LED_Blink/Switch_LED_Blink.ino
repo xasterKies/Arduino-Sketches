3f8625c7efd03c0d3bb20934e525f9f63c4f047c
TiokengSamuel <samueledward40@gmail.com>
2021-02-23 16:32:28 +0100

    The capacitor is connected to the arduino and tested

--- Switch_LED_Blink/Switch_LED_Blink.ino
+++ Switch_LED_Blink/Switch_LED_Blink.ino
@@ -5,6 +5,9 @@ int LEDpin = 4;    //define LED pin
 int reading;   //define reading as an integer
 int switchState = LOW;
 int LEDState = LOW;
+unsigned long switchTime;
+int lastSwitch = LOW;
+int debounceTime = 50;
 
 void setup() {
   pinMode(LEDpin, OUTPUT);
@@ -12,8 +15,18 @@ void setup() {
 
 void loop() {
   reading = digitalRead(switchPin);  //read switch pin
-  if(reading == HIGH && switchState == LOW) LEDState = !LEDState;
-  digitalWrite(LEDpin, reading);     //turn LED on if switch is HIGH
-  switchState = reading;
+  
+  if(reading != lastSwitch)
+  {
+  switchTime = millis();
+  lastSwitch = reading;
+  reading = digitalRead(switchPin);
+  }
+  if((millis() - switchTime) > debounceTime)
+  {
+    if(reading == HIGH && switchState == LOW) LEDState = !LEDState;
+    digitalWrite(LEDpin, reading);     //turn LED on if switch is HIGH
+    switchState = reading;
+  }
   
 }                                    //turn LED off if switch is LOW
